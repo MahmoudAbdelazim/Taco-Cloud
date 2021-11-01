67fd109cca87349e69e81593b782dbbcd9882db2
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2021-11-01 02:12:47 +0200

    Add form validation for taco design and order

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -3,14 +3,13 @@ package tacos;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient.Type;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -18,10 +17,10 @@ import java.util.stream.Collectors;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    @GetMapping
-    public String showDesignForm(Model model) {
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
                 new Ingredient("CARN", "Carnitas", Type.PROTEIN),
@@ -32,13 +31,12 @@ public class DesignTacoController {
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
+
         Type[] types = Ingredient.Type.values();
-        for (Type type: types) {
-            model.addAttribute(type.toString().toLowerCase(Locale.ROOT),
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
-        model.addAttribute("design", new Taco());
-        return "design";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
@@ -47,8 +45,17 @@ public class DesignTacoController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping
+    public String showDesignForm(Model model) {
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
     @PostMapping
-    private String processDesign(Taco design) {
+    private String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
--- src/main/java/tacos/OrderController.java
+++ src/main/java/tacos/OrderController.java
@@ -3,9 +3,13 @@ package tacos;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -19,7 +23,10 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
